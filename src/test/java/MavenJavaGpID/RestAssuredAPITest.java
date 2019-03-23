13ccc1b650616e10cecfc18c76051730f8daf4a8
jakharsuneel <jakhar.suneel@gmail.com>
2019-03-23 15:05:37 -0400

    My 2nd commit for cloned repository

--- src/test/java/MavenJavaGpID/RestAssuredAPITest.java
+++ src/test/java/MavenJavaGpID/RestAssuredAPITest.java
@@ -8,6 +8,8 @@ public class RestAssuredAPITest {
 	public void RestAssuredUITest()
 	{
 	System.out.println("This is RestAssured Test");
+	System.out.println("This is RestAssured Test2");
+	System.out.println("This is RestAssured Test3");
 	}
 
 }
